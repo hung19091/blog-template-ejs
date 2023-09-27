291ff2e7f696e9c49309a92f3a8e31d563ab6182
hung19091 <88091495+hung19091@users.noreply.github.com>
2023-09-27 15:14:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ const composeContent = "Leave me some message?"
 
 const app = express();
 
-// set up rate limiter: maximum of five requests per minute
-const RateLimit = require('express-rate-limit');
-const limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
-});
-// apply rate limiter to all requests
-app.use(limiter);
-
 app.locals._ = _
 
 app.set('view engine', 'ejs');
